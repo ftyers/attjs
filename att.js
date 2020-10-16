bd2478ae89ca8d0a891283708871418bb06a5e29
Francis Tyers <ftyers@prompsit.com>
2020-10-16 22:04:16 +0100

    attjs

---
+++ att.js
@@ -0,0 +1,26 @@
+class ATT {
+  constructor(t) { 
+    console.log('ATT:' + t.length);
+    this.t = t;
+  }
+
+  step(c) { 
+    console.log('step:' + c);
+  }
+
+  closure() { 
+    console.log('closure:');
+
+  }
+
+  lookup(s) { 
+    console.log('lookup:' + c);
+
+  }
+
+  invert() { 
+    console.log('invert:');
+  }
+}
+
+
